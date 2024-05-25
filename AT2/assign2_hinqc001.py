7d94ce6df571434513e8f4c759f7d4304ac4d4b4
TheToastGuy <hinqc001@mymail.unisa.edu.au>
2024-05-25 18:44:44 +0930

    done stage 5, just need to validate all input and ensure it matches sample output

--- AT2/assign2_hinqc001.py
+++ AT2/assign2_hinqc001.py
@@ -15,6 +15,7 @@ import blackjack
 '''     TO-DO'''
 #fix display playter (stage 2)
 #validate player names in interactive mode
+#check index for output is correct
 
 
 """                Functions                """
@@ -89,73 +90,131 @@ def display_players(player_list):
 
 #takes plater name and list of players and returns the index of that player (returns -1 if not found)
 def find_player(player_list, name):
-    pass
+    length = len(player_list)
+    for i in range(length):
+        if player_list[i][0] == name:
+            return i
+    return -1
 
 #updates the plater chip balance. promts user for for the amount of chips they would like to buy (1-100)and updates balance
 #validates the number of chips, displays a message indicating it has been done and an error message if the player is not found
 def buy_player_chips(player_list, name):
-    pass
+    index = find_player(player_list, name)
+    if index == -1:
+        print('Sorry that player does not exist\n')
+        return -1
+
+    done = False
+    while not done:
+        chips = int(input('Please enter the number of chips you would like to buy (1-100): '))
+        if chips < 1 or chips > 100:
+            print('Invalid input, please enter a number between 1 and 100')
+        else:
+            player_list[index][5] += chips
+            print(f'{name} has bought {chips} chips')
+            done = True
+
 
 #displays the plater with the highest chip vbalance. if two players have the same the one with the lower games is displayed. +
 #displays error message if no players are found or if all players have a balance of 0
-def display_highest_chip_holder(player_list):
-    pass
+def display_highest_chip_holder(player_list,silent = False):
+    length = len(player_list)
+    max_val = 0
+    max_index = 0
+    for x in range(length):
+        if player_list[x][5] > max_val:
+            max_val = player_list[x][5]
+            max_index = x
+        elif player_list[x][5] == max_val:
+            if player_list[x][1] < player_list[max_index][1]:
+                max_val = player[5]
+
+    if silent == False:#if silent is true the function is being called by another function and should not display text
+        if max_val == 0:
+            print('Error - no players or all players have 0 chips')
+        else:
+            print(f'the player with the most chips is {player_list[max_index][0]} with {max_val} chips')
+
+    return max_index
+
 
 #adds player name to list. if player name already exists error is displayed.
 #uses find_player() to check if name already exists first
 #chip balanace starts at 100, all other stats start at 0
 def add_player(player_list, name):
-    pass
+    find_player(player_list, name)
+    if find_player(player_list, name) != -1:
+        print('Sorry that player already exists\n')
+        return -1
+    player_list.append([name, 0, 0, 0, 0, 100, 0])
+    print('{} has been added succsesfully'.format(name))
 
 #removes player from list and displays text indicating this has been done
 #if player isnt found an error message is returned
 #calls find_player()
-def remove_player(player_list, name):
-    pass
+def remove_player(player_list, name, silent = False):#silent is used to stop the function from displaying text when called by another function
+
+    index = find_player(player_list, name)
+    if index == -1:
+        if silent == False:
+            print('Sorry that player does not exist\n')
+        return -1
+    else:
+        updated_list = [] #i am not allowed to use in built functions to remove this player. so a new list is created with all players but the one to be removed
+        length = len(player_list)
+        for i in range(length):
+            if i != index:
+                updated_list.append(player_list[i])
+        if silent == False:
+            print('{player} has been succsesfully removed'.format(player = name))
+        return updated_list
+
 
 #allows the player to play BlackJack agasint a computer untill they enter n to 'Play again [Y|N]?
 #stats are updated after everyround -  +3 points for player win, +1 point for player draw and 0 points awarded for playe loss
 #calls blackjack.play_one_game()
 def play_blackjack_games(player_list, player_pos):
-    pass
+    play = 'y'
+    while play.lower() == 'y':
+        player_list[player_pos][1] += 1#adds one to games played
+        result, chips = blackjack.play_one_game(player_list[player_pos][5])
+
+        player_list[player_pos][5] = chips#updates chip balance
+        player_list[player_pos][6] += result#updates score
+
+        if result == 3:
+            player_list[player_pos][2] += 1
+        elif result == 1:
+            player_list[player_pos][4] += 1
+        else:
+            player_list[player_pos][3] += 1
+
+
+
+        play = input('Play again [Y|N]?')
+
+
 #returrns a copy of player list in desecnding order of chip balance
 #when two players have the same balance the one with the lower amount of games played appears first
-
 def sort_by_chips (player_list):
-    pass
+    temp_list = []
+    new_sorted_list = []
+    for item in player_list:
+        temp_list.append(item)
+
+    while len(temp_list) > 0:
+        index = display_highest_chip_holder(temp_list, True)
+        new_sorted_list.append(temp_list[index])
+        temp_list = remove_player(temp_list, temp_list[index][0], True)
+
+    return new_sorted_list
+
+
+
 
 
 """                Functions other then the required 10                """
 
-#allows the user to navigate through the program
-def interactive_mode():
-    done = False
-    while not done:
-        done = True
-        u_input = str(input('please enter choice \n[list, buy, search, high, add, remove, play, chips, quit]: ')).lower()
-
-        if u_input == 'list':
-            display_players(player_list)
-        elif u_input == 'buy':
-            buy_player_chips(player_list, input('please enter player name: '))
-        elif u_input == 'search':
-            find_player(player_list, input('please enter player name: '))
-        elif u_input == 'high':
-            display_highest_chip_holder(player_list)
-        elif u_input == 'add':
-            add_player(player_list, input('please enter player name: '))
-        elif u_input == 'remove':
-            remove_player(player_list, input('please enter player name: '))
-        elif u_input == 'play':
-            play_blackjack_games(player_list, input('please enter player name: '))
-        elif u_input == 'chips':
-            sort_by_chips(player_list)
-        elif u_input == 'quit':
-            pass
-            write_to_file("players.txt", player_list)
-        else:
-            print('invalid input\n')
-            done = False
 
 
 
@@ -166,7 +225,63 @@ player_list = []
 """                main code                """
 
 player_list = read_file("players.txt")
-interactive_mode()
+
+
+done = False
+while not done:
+    #done = True
+    u_input = str(input('please enter choice \n[list, buy, search, high, add, remove, play, chips, quit]: ')).lower()
+
+    if u_input == 'list':
+        display_players(player_list)
+
+    elif u_input == 'buy':
+        player = str(input('please enter player name: '))
+        buy_player_chips(player_list, player)
+
+
+    elif u_input == 'search':
+        index = find_player(player_list, input('please enter player name: '))
+
+        if index == -1:
+            print('Sorry that player does not exist\n')
+        else:
+           print('Player name:', player_list[index][0], '\nGames Played:', player_list[index][1], '\nGames won:', player_list[index][2], '\nGames lost:', player_list[index][3], '\nGames drawn:', player_list[index][4], '\nChips:', player_list[index][5], '\nScore:', player_list[index][6])
+
+    elif u_input == 'high':
+        display_highest_chip_holder(player_list)
+
+    elif u_input == 'add':
+        add_player(player_list, input('please enter player name: '))
+
+    elif u_input == 'remove':
+        temp = remove_player(player_list, input('please enter player name: '))
+        if temp != -1:
+            player_list = temp
+
+
+
+    elif u_input == 'play':
+        index = find_player(player_list, input('please enter player name: '))
+        if index == -1:
+            print('Sorry that player does not exist')
+        else:
+            play_blackjack_games(player_list, index)
+
+    elif u_input == 'chips':
+        desc_list = []
+        desc_list = sort_by_chips(player_list)
+        display_players(desc_list)
+
+    elif u_input == 'quit':
+        done = True
+        print("\n\n-- Program terminating --\n")
+        write_to_file("players.txt", player_list)
+
+    else:
+        print('invalid input\n')
+        #done = False
+
 
 
 
